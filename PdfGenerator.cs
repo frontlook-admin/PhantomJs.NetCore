4dd71338d5cd56dffbdb9c218191eac6e214b2a9
FrontLook-Admin <frontlook.inc@gmail.com>
2020-05-24 00:20:41 +0530

    Primary

--- PdfGenerator.cs
+++ PdfGenerator.cs
@@ -3,127 +3,104 @@ using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
 
-namespace PhantomJs.NetCore
-{
-    public interface IHtmlToPdfConverter
-    {
-        /// <summary>
-        /// Render the specified HTML code to a PDF.
-        /// Save that PDF to the specified directory.
-        /// </summary>
-        /// <param name="html">The HTML to convert to PDF.</param>
-        /// <param name="outputFolder">The directory to save the PDF to.</param>
-        /// <returns>The full file path of the generated PDF.</returns>
-        string Convert(string html, string outputFolder);
-    }
+namespace PhantomJs.NetCore {
+    public class PdfGenerator {
 
-    public class PhantomJsHtmlToPdfConverter : IHtmlToPdfConverter
-    {
-        private readonly OS _platform;
-        private readonly PdfGeneratorOptions _options;
+        private string PhantomRootFolder;
+        private OS Platform;
 
-        public PhantomJsHtmlToPdfConverter(PdfGeneratorOptions options)
-        {
-            _options = options ?? throw new ArgumentNullException(nameof(options));
-            _platform = GetOsPlatform();
+        public PdfGenerator(string phantomRootFolder) {
+
+            if(!Directory.Exists(phantomRootFolder)) {
+                throw new ArgumentException(String.Format("Invalid Path: No such folder exists: {0}",phantomRootFolder));
+            }
+            this.PhantomRootFolder = phantomRootFolder;
+            this.Platform = GetOsPlatform();
         }
 
-        /// <summary>
-        /// Render the specified HTML code to a PDF.
-        /// Save that PDF to the specified directory.
-        /// </summary>
-        /// <param name="html">The HTML to convert to PDF.</param>
-        /// <param name="outputFolder">The directory to save the PDF to.</param>
-        /// <returns>The file name of the generated PDF.</returns>
-        public string Convert(string html, string outputFolder)
-        {
-            // Write the passed html in a file.
-            var htmlFileName = WriteHtmlToTempFile(html);
-            try
-            {
-                if (!Directory.Exists(outputFolder))
-                {
-                    throw new ArgumentException("The output folder is not a valid directory!");
-                }
-
-                var phantomExeToUse =
-                    (_platform == OS.LINUX) ? "linux64_phantomjs.exe" :
-                    (_platform == OS.WINDOWS) ? "windows_phantomjs.exe" :
-                    "osx_phantomjs.exe";
-
-                return ExecutePhantomJs(phantomExeToUse, htmlFileName, outputFolder);
-            }
-            finally
-            {
-                File.Delete(Path.Combine(_options.PhantomRootFolder, htmlFileName));
-            }
+        enum OS {
+            LINUX,
+            WINDOWS,
+            OSX
         }
 
-        private OS GetOsPlatform()
-        {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
+        private OS GetOsPlatform() {
+            if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
                 return OS.WINDOWS;
             }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                return OS.LINUX;
-            }
-            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            {
+        	else if(RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) {
+        		return OS.LINUX;
+        	}
+            else if(RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
                 return OS.OSX;
             }
 
             throw new Exception("PdfGenerator: OS Environment could not be probed, halting!");
         }
 
-        private string ExecutePhantomJs(string phantomJsExeToUse, string inputFileName, string outputFolder)
-        {
+        private string ExecutePhantomJs(string phantomJsExeToUse,string inputFileName, string outputFolder) {
+
             // The output file must be located in the output folder.
-            var outputFilePath = Path.Combine(outputFolder, $"{inputFileName}.pdf");
+            string outputFilePath = Path.Combine(outputFolder, String.Format("{0}.pdf", inputFileName));
+            
 
-            var phantomJsAbsolutePath = Path.Combine(_options.PhantomRootFolder, phantomJsExeToUse);
-            var startInfo = new ProcessStartInfo(phantomJsAbsolutePath)
-            {
-                WorkingDirectory = _options.PhantomRootFolder,
-                Arguments = $"rasterize.js \"{inputFileName}\" \"{outputFilePath}\" \"{_options.PaperSize}\"",
-                UseShellExecute = false
-            };
+            string phantomJsAbsolutePath = Path.Combine(this.PhantomRootFolder,phantomJsExeToUse);
+            ProcessStartInfo startInfo = new ProcessStartInfo(phantomJsAbsolutePath);
+            startInfo.WorkingDirectory = this.PhantomRootFolder;
+            startInfo.Arguments = 
+                String.Format("rasterize.js \"{0}\" {1} \"A4\"",inputFileName,outputFilePath);
+            startInfo.UseShellExecute = false;
 
-            var proc = new Process() { StartInfo = startInfo };
+            Process proc = new Process() { StartInfo = startInfo };
             proc.Start();
+
             proc.WaitForExit();
+
             return outputFilePath;
         }
 
-        private string WriteHtmlToTempFile(string html)
-        {
-            var filename = Path.GetRandomFileName() + ".html";
-            var absolutePath = Path.Combine(_options.PhantomRootFolder, filename);
+        private string WriteHtmlToTempFile(string html) {
+            string filename = Path.GetRandomFileName() + ".html";
+
+            string absolutePath = Path.Combine(this.PhantomRootFolder, filename);
+
             File.WriteAllText(absolutePath, html);
+
             return filename;
         }
 
-        private enum OS
-        {
-            LINUX,
-            WINDOWS,
-            OSX
+        /**
+            This function takes in an 'html' string and generates a pdf
+            containing its rendered version, and writes the pdf to the passed
+            output 'outputFolder'.
+
+            
+         */
+        public string GeneratePdf(string html, string outputFolder) {
+
+            if(!Directory.Exists(outputFolder)) {
+                throw new ArgumentException("The output folder is not a valid directory!");
+            }
+
+
+            string phantomExeToUse =
+                (this.Platform == OS.LINUX) ? "linux64_phantomjs.exe" :
+                (this.Platform == OS.WINDOWS) ? "windows_phantomjs.exe" :
+                "osx_phantomjs.exe";
+            
+            // Write the passed html in a file.
+            string htmlFileName = WriteHtmlToTempFile(html);
+
+            string pdfFileName = "";
+            try {
+                pdfFileName = ExecutePhantomJs(phantomExeToUse,htmlFileName,outputFolder);
+            } finally {
+                // delete the temp file.
+                File.Delete(Path.Combine(this.PhantomRootFolder,htmlFileName));
+            }
+
+
+            return pdfFileName;
         }
-    }
-
-    public class PdfGeneratorOptions
-    {
-        public const string DefaultPaperSize = "Letter";
-
-        public PdfGeneratorOptions(string phantomRootFolder)
-        {
-            if (string.IsNullOrWhiteSpace(phantomRootFolder)) { throw new ArgumentNullException(nameof(phantomRootFolder)); }
-            if (!Directory.Exists(phantomRootFolder)) { throw new ArgumentException($"Invalid Path: No such folder exists: {phantomRootFolder}"); }
-            PhantomRootFolder = phantomRootFolder;
-        }
-
-        public string PhantomRootFolder { get; set; }
-        public string PaperSize { get; set; } = DefaultPaperSize;
     }
 }
